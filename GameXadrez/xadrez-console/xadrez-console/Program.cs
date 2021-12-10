ded96307f683dcb2068cd2b84456e92749802f87
Lucas Rosa <lucasdarosasd@gmail.com>
2021-12-10 20:02:52 -0300

    vo1.07 - Ajuste posição

--- GameXadrez/xadrez-console/xadrez-console/Program.cs
+++ GameXadrez/xadrez-console/xadrez-console/Program.cs
@@ -11,20 +11,7 @@ namespace xadrez_console
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
 
-            try
-            {
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(9, 9));
-
-                Tela.imprimirTabuleiro(tab);
-
-            }
-
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.ReadLine();
         }
     }
 }
