02445de0545960154a8d781d88dc75194bb6862a
Lucas Rosa <lucasdarosasd@gmail.com>
2021-12-10 20:20:51 -0300

    v01.08 - Add melhorias

--- GameXadrez/xadrez-console/xadrez-console/Program.cs
+++ GameXadrez/xadrez-console/xadrez-console/Program.cs
@@ -11,7 +11,21 @@ namespace xadrez_console
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.ReadLine();
+            try
+            {
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(7, 2));
+
+                Tela.imprimirTabuleiro(tab);
+
+
+            }
+
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
